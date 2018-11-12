9ed1b1210f1e2bb2c5b0f39b6a6b0ee0cf935acd
Bako Farkas <farkas.bako@gmail.com>
2018-11-12 23:45:35 +0100

    Objects and classes videos

---
+++ week-3/day-11/src/Earth.java
@@ -0,0 +1,19 @@
+public class Earth {
+    public static void main(String[] args) {
+
+
+        Human archie;
+
+        archie = new Human();
+        archie.age = 30;
+        archie.height = 180;
+        archie.eyecolor = "brown";
+        archie.name = "Archie";
+
+        archie.speak();
+
+
+    }
+
+
+}
---
+++ week-3/day-11/src/Human.java
@@ -0,0 +1,23 @@
+public class Human {
+
+    String name;
+    int age;
+    int height;
+    String eyecolor;
+
+    public Human(){
+
+
+    }
+
+
+
+    public void speak() {
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am " + height + " centimeters tall");
+        System.out.println("My eyecolor is " + eyecolor);
+
+    }
+
+}
