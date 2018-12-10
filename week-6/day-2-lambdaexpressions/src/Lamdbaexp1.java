e3d370f33b8583a0e1fac5e601d3c520df4db55a
Bako Farkas <farkas.bako@gmail.com>
2018-12-10 14:01:30 +0100

    lambda exp exercise 6

--- week-6/day-2-lambdaexpressions/src/Lamdbaexp1.java
+++ week-6/day-2-lambdaexpressions/src/Lamdbaexp1.java
@@ -61,6 +61,11 @@ public class Lamdbaexp1 {
         System.out.println(stats.getAverage());
 
 
+        //Write a Stream Expression to get the sum of the odd numbers in the following list:
+
+        List<Integer> numbers5 = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+
+
 
 
 
